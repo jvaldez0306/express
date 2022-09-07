92e472eaef5cf31529a756e58628be07c272b68e
Jose Valdez <jose.avaldezz0306@gmail.com>
2022-09-07 00:31:10 -0400

    Se agregan archivos

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,7 +20,7 @@ exports.addProduct = (req, res) => {
     fs.readFileSync(`${__dirname}/../data/products.json`)
   );
   products.push(req.body);
-  fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
+  fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
 
   res.status(200).json({
     status: "success",
@@ -34,15 +34,58 @@ exports.getProductById = (req, res) => {
   const products = JSON.parse(
     fs.readFileSync(`${__dirname}/../data/products.json`)
   );
-
   const foundProduct = products.find((p) => p.id == req.params.id);
   if (foundProduct) {
-    res.status(200).json({
+    return res.status(200).json({
       status: "success",
       data: {
         product: foundProduct,
       },
     });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
+
+
+exports.deleteProductById = (req, res) => {
+
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );  
+  const deleted = products.find((p) => p.id == req.params.id);
+  if (deleted) {
+      return res.status(200).json({
+      data: {
+       products : products.filter((p) => p.id != req.params.id)}
+      
+    });
+
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
+
+exports.putProductById = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+  products.push(req.body);
+  fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+
+  const put = products.find((p) => p.id == req.params.id);
+  if (put) {
+    return res.status(200).json({
+      status: "success",
+      data: {products
+        }
+
+    });
+    
   } else {
     res.status(404).json({
       status: "not found",
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const productController = require("./../controllers/productController");
 const productRouter = express.Router();
+
+
 //routes
 productRouter
   .route("/")
   .get(productController.getAllProducts)
   .post(productController.addProduct);
-productRouter.route("/:id").get(productController.getProductById);
+productRouter
+.route("/:id")
+.get(productController.getProductById)
+.delete(productController.deleteProductById)
+.put(productController.putProductById);
 
 module.exports = productRouter;

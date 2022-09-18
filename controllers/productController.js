2019439c7bd33170330c8661f32e04ce2fe6df03
Jose Valdez <jose.avaldezz0306@gmail.com>
2022-09-18 15:26:50 -0400

    remplazando archivos por  mongoose en controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
+const Product = require("../models/Product");
 
-exports.getAllProducts = (req, res) => {
-  const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/products.json`)
-  );
-
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  
   res.status(200).json({
     status: "success",
     timeOfRequest: req.requestTime,
@@ -15,26 +14,18 @@ exports.getAllProducts = (req, res) => {
   });
 };
 
-exports.addProduct = (req, res) => {
-  const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/products.json`)
-  );
-  products.push(req.body);
-  fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
-
+exports.addProduct = async(req, res) => {
+  const newProduct = await Product.create(req.body);
   res.status(200).json({
     status: "success",
     data: {
-      products,
+      products: newProduct ,
     },
   });
 };
 
-exports.getProductById = (req, res) => {
-  const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/products.json`)
-  );
-  const foundProduct = products.find((p) => p.id == req.params.id);
+exports.getProductById = async(req, res) => {
+  const foundProduct = await Product.findById(req.params.id);
   if (foundProduct) {
     return res.status(200).json({
       status: "success",
@@ -51,26 +42,33 @@ exports.getProductById = (req, res) => {
 
 
 exports.deleteProductById = (req, res) => {
-
   const products = JSON.parse(
     fs.readFileSync(`${__dirname}/../data/products.json`)
   );  
+  /*  fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products)); */
   const deleted = products.find((p) => p.id == req.params.id);
-  if (deleted) {
-      return res.status(200).json({
-      data: {
-       products : products.filter((p) => p.id != req.params.id)}
-      
-    });
+/*   products.filter((p) => p.id != req.params.id); */
+  products.filter(req.body);
+ /* fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products)); */
 
-  } else {
-    res.status(404).json({
-      status: "not found",
-    });
-  }
+ if (deleted) {
+  return res.status(200).json({
+    status: "product was deleted",
+    data: {
+      products,
+    },
+  });
+} else {
+  res.status(404).json({
+    status: "not found",
+  });
+}
 };
 
-exports.putProductById = (req, res) => {
+
+
+ 
+exports.updateProductById = (req, res) => {
   const products = JSON.parse(
     fs.readFileSync(`${__dirname}/../data/products.json`)
   );
